eb77ad8081a7fbe1c7ede8882b650c69bece7012
Hugo Ernesto Sosa <hsosa@cnea.gov.ar>
2024-04-30 09:40:58 -0300

    se pasa la cuenta a minutos y se cambian los msj de resultados

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ function calculateHours() {
             const hours = (endTime - startTime) / (1000 * 60 * 60);
 
             const dayElement = document.getElementById(day.toLowerCase() + '-output');
-            dayElement.innerHTML = '';
+            dayElement.innerHTML = ' ';
             if (hours >= 4) {
-                dayElement.innerHTML += ' ✔️';
+                dayElement.innerHTML += '✔️';
             } else {
-                dayElement.innerHTML = '';
+                dayElement.innerHTML = ' ';
             }
 
             totalHours += hours;
@@ -30,13 +30,13 @@ function calculateHours() {
     resultDiv.innerHTML = `Total de horas: ${totalHours.toFixed(2)}<br>`;
 
     const requiredHours = 8 * completedDays;
-    if (totalHours >= requiredHours) {
-        const remainingHoursF_hs = (totalHours - requiredHours).toFixed(0);
-        const remainingHoursF_min = (((totalHours - requiredHours).toFixed(2)-remainingHoursF_hs)*60).toFixed(0);
-        resultDiv.innerHTML += `Cumple con ${requiredHours} horas diarias, tenes ${remainingHoursF_hs} hs y ${remainingHoursF_min} min a favor <br>`;
+    if (totalHours*60 >= requiredHours*60) {
+        const remainingHoursF_hs = (totalHours*60 - requiredHours*60).toFixed(0);
+        const remainingHoursF_min = (totalHours*60 - requiredHours*60).toFixed(0);
+        resultDiv.innerHTML += `¡Sos Crack! 😎, tenés ${(remainingHoursF_hs/60).toFixed(0)} hs y ${remainingHoursF_min} min a favor <br>`;
     } else {
-        const remainingHours_hs = (requiredHours - totalHours).toFixed(0);
-        const remainingHours_min = (((requiredHours - totalHours).toFixed(2)-remainingHours_hs)*60).toFixed(0);
-        resultDiv.innerHTML += `Faltan ${remainingHours_hs} hs y ${remainingHours_min} min para cumplir con ${requiredHours} horas diarias<br>`;
+        const remainingHours_hs = (requiredHours*60 - totalHours*60).toFixed(0);
+        const remainingHours_min = (requiredHours*60 - totalHours*60).toFixed(0);
+        resultDiv.innerHTML += `¡Estas al horno!, te faltan ${(remainingHours_hs/60).toFixed(0)} hs y ${remainingHours_min} min para cumplir con ${requiredHours} horas diarias<br>`;
     }
 }
