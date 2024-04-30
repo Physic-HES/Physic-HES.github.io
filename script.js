bc815834866275c6a41ae71d6b8b7363ec029f85
Hugo Ernesto Sosa <hsosa@cnea.gov.ar>
2024-04-30 10:05:16 -0300

    ahora si se agrega espacio

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function calculateHours() {
     });
 
     const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = ` <br>`;
+    resultDiv.innerHTML = ` \n <br>`;
 
     const requiredHours = 8 * completedDays;
     if (totalHours*60 >= requiredHours*60) {
