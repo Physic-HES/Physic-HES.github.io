b98445792721543843e545f2d9e2bc7ac920f23c
Hugo Ernesto Sosa <hsosa@cnea.gov.ar>
2024-04-30 09:57:32 -0300

    se corrige cuenta de minutos a deuda

--- script.js
+++ script.js
@@ -31,12 +31,12 @@ function calculateHours() {
 
     const requiredHours = 8 * completedDays;
     if (totalHours*60 >= requiredHours*60) {
-        const remainingHoursF_hs = (totalHours*60 - requiredHours*60).toFixed(0);
+        const remainingHoursF_hs = totalHours*60 - requiredHours*60;
         const remainingHoursF_min = (totalHours*60 - requiredHours*60).toFixed(0);
         resultDiv.innerHTML += `¡Sos Crack! 😎, tenés ${(remainingHoursF_hs/60).toFixed(0)} hs y ${remainingHoursF_min} min a favor <br>`;
     } else {
-        const remainingHours_hs = (requiredHours*60 - totalHours*60).toFixed(0);
+        const remainingHours_hs = requiredHours*60 - totalHours*60;
         const remainingHours_min = (requiredHours*60 - totalHours*60).toFixed(0);
-        resultDiv.innerHTML += `¡Estas al horno!, te faltan ${(remainingHours_hs/60).toFixed(0)} hs y ${remainingHours_min} min para cumplir con ${requiredHours} horas diarias<br>`;
+        resultDiv.innerHTML += `¡Estas al horno!, te faltan ${(remainingHours_hs/60).toFixed(0)} hs y ${remainingHours_min-(remainingHours_hs/60).toFixed(0)*60} min para cumplir con ${requiredHours} horas diarias<br>`;
     }
 }
