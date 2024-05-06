b2b9809a6c17044fdb97c840b211080dbcd9b4a7
Hugo Ernesto Sosa <hsosa@cnea.gov.ar>
2024-05-06 12:37:10 -0300

    se agrega prediccion de salida

--- script.js
+++ script.js
@@ -23,6 +23,10 @@ function calculateHours() {
 
             totalHours += hours;
             completedDays++;
+        } else if (start && !end) {
+            const reftime = new Date('1970-01-01T00:00')
+            const suggestedTime = new Date('1970-01-01T' + start);
+            suggestedHours = 8+(suggestedTime-reftime)/(1000*60*60)-(totalHours-8*completedDays);
         }
     });
 
@@ -30,13 +34,18 @@ function calculateHours() {
     resultDiv.innerHTML = ` \n <br>`;
 
     const requiredHours = 8 * completedDays;
-    if (totalHours*60 >= requiredHours*60) {
-        const remainingHoursF_hs = totalHours*60 - requiredHours*60;
-        const remainingHoursF_min = Math.trunc(totalHours*60 - requiredHours*60);
-        resultDiv.innerHTML += `¡Sos Crack! 😎, Tenés ${Math.trunc(remainingHoursF_hs/60)} hs y ${((Math.trunc(remainingHoursF_min)/60-Math.trunc(remainingHoursF_hs/60))*60).toFixed(0)} min a favor <br>`;
-    } else {
-        const remainingHours_hs = requiredHours*60 - totalHours*60;
-        const remainingHours_min = Math.trunc(requiredHours*60 - totalHours*60);
-        resultDiv.innerHTML += `¡Estas al horno! 😬, Te faltan ${Math.trunc(remainingHours_hs/60)} hs y ${((Math.trunc(remainingHours_min)/60-Math.trunc(remainingHours_hs/60))*60).toFixed(0)} min para cumplir con las ${Math.trunc(requiredHours)} horas<br>`;
+    if (completedDays) {
+        if (totalHours*60 >= requiredHours*60) {
+            const remainingHoursF_hs = totalHours*60 - requiredHours*60;
+            const remainingHoursF_min = Math.trunc(totalHours*60 - requiredHours*60);
+            resultDiv.innerHTML += `¡Sos Crack! 😎, Tenés ${Math.trunc(remainingHoursF_hs/60)} hs y ${((Math.trunc(remainingHoursF_min)/60-Math.trunc(remainingHoursF_hs/60))*60).toFixed(0)} min a favor <br>`;
+        } else {
+            const remainingHours_hs = requiredHours*60 - totalHours*60;
+            const remainingHours_min = Math.trunc(requiredHours*60 - totalHours*60);
+            resultDiv.innerHTML += `¡Estas al horno! 😬, Te faltan ${Math.trunc(remainingHours_hs/60)} hs y ${((Math.trunc(remainingHours_min)/60-Math.trunc(remainingHours_hs/60))*60).toFixed(0)} min para cumplir con las ${Math.trunc(requiredHours)} horas<br>`;
+        }
+    }
+    if (suggestedHours) {
+        resultDiv.innerHTML += `Deberías salir a las ${Math.trunc(suggestedHours)}:${((suggestedHours-Math.trunc(suggestedHours))*60).toFixed(0)} hs. No cuelgues! afuera hay una vida<br>`;
     }
 }
